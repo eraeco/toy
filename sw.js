9a62d17113893f73bee1a9f0a911ea9ce98c56a9
Mars <mars@gun.eco>
2026-03-14 14:28:02 -0700

    make installable

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Minimal SW for installability, just fetch through
+  e.respondWith(fetch(e.request));
+});
